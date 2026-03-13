72fdbf661f1bc5c7fb39443bb701ca2357af6214
fgdamasceno <fgdamasceno@gmail.com>
2026-03-12 21:22:07 -0300

    primeiro commit e primeiro programa

---
+++ README.md
@@ -0,0 +1,9 @@
+# Java - Estudos
+
+Repositório dedicado ao estudo da linguagem Java.
+
+Curso principal:
+Java COMPLETO Programação Orientada a Objetos + Projetos — Nélio Alves
+
+Objetivo:
+Dominar Java e backend para atuar como desenvolvedor profissional.
---
+++ logica_da_programacao/sec03_01-primeiro-programa/src/Program.java
@@ -0,0 +1,15 @@
+// METODO ANTIGO PRE JAVA 25
+//public class Program {
+//	
+//	public static void main(String[] args) {
+//		
+//		System.out.println("Olá, Mundo!");
+//		
+//	}
+//
+//}
+
+// METODO ATUAL A PARTIR DO JAVA 25
+void main() {
+	IO.println("Olá, Mundo!");
+}
