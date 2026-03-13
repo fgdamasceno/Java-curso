910e2cd6f3bcb745e5fb010ff57183eb85f1b1bf
fgdamasceno <fgdamasceno@gmail.com>
2026-03-13 14:57:16 -0300

    leitura e escrita de dados

---
+++ logica_da_programacao/sec04_03-estrutura-sequencial/src/Program.java
@@ -0,0 +1,48 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		// AULA ENTRADA DE DADOS
+		Scanner sc = new Scanner(System.in);
+		
+		String name;
+		int anoNascimento, anoAtual, idade;
+		double salario;
+		
+		anoAtual = 2026;
+		
+		System.out.println("Digite o seu nome:");
+		name = sc.next(); // recebe String
+		
+		System.out.println("Digite seu ano de nascimento:");
+		anoNascimento = sc.nextInt(); // recebe int
+		
+		System.out.println("Digite o seu salário:");
+		salario = sc.nextDouble(); // recebe double
+		idade = anoAtual - anoNascimento;
+		
+		System.out.printf("%nO nome digitado é %s.", name);
+		System.out.printf("%nIdade é %d.", idade);
+		System.out.printf("%nO salário digitado é $ %.2f.", salario);
+		
+		// Entrada de dados até a quebra de linha - o usuário aperta a tecla ENTER
+		String nomeCompleto;
+		
+		System.out.println("Digite seu nome completo:");
+		sc.nextLine();
+		nomeCompleto = sc.nextLine();
+		
+		System.out.printf("Nome Completo: %s%n", nomeCompleto);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
