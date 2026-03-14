f85c786055859ae27b5b229f0a0bbc87d0883629
fgdamasceno <fgdamasceno@gmail.com>
2026-03-14 19:58:14 -0300

    switch-case

---
+++ logica_da_programacao/sec05_04-estrutura-condicional/src/Program.java
@@ -0,0 +1,39 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		// switch-case
+		
+		int diaSemana = 18;
+		
+		switch (diaSemana) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terça");
+			break;
+		case 4:
+			System.out.println("Quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sábado");
+			break;
+		default:
+			System.out.println("Dia inválido");
+			break;
+		}
+
+	}
+
+}
