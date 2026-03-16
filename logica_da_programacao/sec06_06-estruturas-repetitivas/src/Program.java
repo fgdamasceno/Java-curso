221b9f3704ef08920860a01a8a797a84fe7079e9
fgdamasceno <fgdamasceno@gmail.com>
2026-03-16 14:59:42 -0300

    do while loop

--- logica_da_programacao/sec06_06-estruturas-repetitivas/src/Program.java
+++ logica_da_programacao/sec06_06-estruturas-repetitivas/src/Program.java
@@ -4,6 +4,9 @@ public class Program {
 	public static void main(String[] args) {
 		
 		// do while
+		// primeiro executo o bloco 'do'
+		// depois a condição é avaliada em 'while'
+		// se 'V', repete o 'do', se 'F' sai do bloco
 		
 		int count = 0;
 		
