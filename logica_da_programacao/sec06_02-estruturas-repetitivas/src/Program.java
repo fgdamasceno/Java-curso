3b38961654030d91b445f02318cd17e9cb16945b
fgdamasceno <fgdamasceno@gmail.com>
2026-03-16 09:59:04 -0300

    aula while loop

--- logica_da_programacao/sec06_02-estruturas-repetitivas/src/Program.java
+++ logica_da_programacao/sec06_02-estruturas-repetitivas/src/Program.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class Program {
+	
+	public static void main(String[] args) {
+		
+		// while loop
+		Scanner sc = new Scanner(System.in);
+		
+		int numero = sc.nextInt();;
+		int soma = 0;
+		
+		while (numero != 0) {
+			soma += numero;
+			numero = sc.nextInt();
+		}
+		
+		System.out.println(soma);
+	
+		sc.close();
+	}
 
 }
