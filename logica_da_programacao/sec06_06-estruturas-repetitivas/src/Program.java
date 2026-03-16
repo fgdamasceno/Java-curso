330f1084a69b8bfba836f46a9c95359b1c4c32dd
fgdamasceno <fgdamasceno@gmail.com>
2026-03-16 14:59:16 -0300

    do while loop

---
+++ logica_da_programacao/sec06_06-estruturas-repetitivas/src/Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		// do while
+		
+		int count = 0;
+		
+		do {
+			System.out.println(count);
+			count++;
+		} while (count < 10);
+		
+	}
+
+}
