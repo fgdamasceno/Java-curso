e65dc5a5a8dff2950837a78655f8d7949452b67c
fgdamasceno <fgdamasceno@gmail.com>
2026-03-25 18:42:10 -0300

    calculo entre datas e duração

--- programação_orientada_a_objetos/sec11_01-data-hora/src/application/Program.java
+++ programação_orientada_a_objetos/sec11_01-data-hora/src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
 	
@@ -99,10 +101,37 @@ public class Program {
 		System.out.println("d05 hora = " + d05.getHour());
 		System.out.println("d05 minuto = " + d05.getMinute());
 		
+		// Calculo com data-hora: d04, d05, d06
 		
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+//		Instant pastWeekLocalInstant = d06.minus(Duration.ofDays(7));
+		Instant pastWeekLocalInstant = d06.minus(7, ChronoUnit.DAYS);
+//		Instant nextWeekLocalInstant = d06.plus(Duration.ofDays(7));
+		Instant nextWeekLocalInstant = d06.plus(7, ChronoUnit.DAYS);
 		
+		System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+		System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+		System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+		System.out.println("pastWeekLocalInstant = " + pastWeekLocalInstant);
+		System.out.println("nextWeekLocalInstant = " + nextWeekLocalInstant);
 		
+		// Duração entre horas: d05
 		
+		// LocalDate, por não ter horas, necessita de conversão
+		Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+//		Duration t1 = Duration.between(pastWeekLocalDate.atTime(0, 0), d04.atTime(0, 0));
+		Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+		Duration t3 = Duration.between(pastWeekLocalInstant, d06);
+		Duration t4 = Duration.between(d06, pastWeekLocalInstant); // valores invertidos
+		
+		System.out.println("Duration t1 = " + t1.toDays());
+		System.out.println("Duration t2 = " + t2.toDays());
+		System.out.println("Duration t3 = " + t3.toDays());
+		System.out.println("Duration t4 = " + t4.toDays());
 		
 	}
 
