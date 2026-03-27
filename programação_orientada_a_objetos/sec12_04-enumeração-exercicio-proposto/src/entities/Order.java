d8584a150c30f2b6068479bb26ec6ab1f412f54c
fgdamasceno <fgdamasceno@gmail.com>
2026-03-27 13:00:15 -0300

    exercicio proposto - minha solução

---
+++ programação_orientada_a_objetos/sec12_04-enumeração-exercicio-proposto/src/entities/Order.java
@@ -0,0 +1,85 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date moment;
+	private OrderStatus status;
+	
+	private Client client;
+	private List<OrderItem> items = new ArrayList<>();
+	
+	public Order() {
+	}
+	
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public void addItem(OrderItem item) {
+		items.add(item);
+	}
+	
+	public void removeItem(OrderItem item) {
+		items.remove(item);
+	}
+
+	public double total() {
+		double sum = 0.0;
+		for (OrderItem i : items) {
+			sum += i.subTotal();
+		}
+		return sum;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("ORDER SUMMARY:\n");
+		sb.append("Order moment: " + sdf.format(moment) + "\n");
+		sb.append("Order status: " + status + "\n");
+		sb.append("Client: " + client.toString()  + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem i : items) {
+			sb.append(i.getProduct().getName()
+					+ ", "
+					+ String.format("$%.2f", i.getPrice())
+					+ ", Quantity: "
+					+ i.getQuantity()
+					+ ", Subtotal: "
+					+ String.format("$%.2f", i.subTotal())
+					+ "\n");			
+		}
+		sb.append(String.format("Total price: $%.2f", total()) + "\n");
+		return sb.toString();
+	}
+	
+	
+
+}
