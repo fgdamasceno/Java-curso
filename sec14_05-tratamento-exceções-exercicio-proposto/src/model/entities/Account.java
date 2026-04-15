9d657b0d67fc35716a8c073b54de53da5a6aa404
fgdamasceno <fgdamasceno@gmail.com>
2026-04-15 11:32:55 -0300

    solução instrutor

--- sec14_05-tratamento-exceções-exercicio-proposto/src/model/entities/Account.java
+++ sec14_05-tratamento-exceções-exercicio-proposto/src/model/entities/Account.java
@@ -45,18 +45,22 @@ public class Account {
 		return withdrawLimit;
 	}
 	
-	public void deposit(Double amount) {
+	public void deposit(double amount) {
 		this.balance += amount;
 	}
 	
-	public void withdraw(Double amount) {
-		if (amount > this.withdrawLimit) {
-			throw new DomainException("The amount exceeds withdraw limit.");
-		}
-		if (amount > this.balance) {
-			throw new DomainException("Not enough balance.");
-		}
+	public void withdraw(double amount) {
+		validateWithdraw(amount);
 		this.balance -= amount;
+	}
+	
+	private void validateWithdraw(double amount) {
+		if (amount > getWithdrawLimit()) {
+			throw new DomainException("Withdraw error: The amount exceeds withdraw limit.");
+		}
+		if (amount > getBalance()) {
+			throw new DomainException("Withdraw error: Not enough balance.");
+		}
 	}
 	
 	
