b567a6e6d2d1acfd707f3e006a98eb2cacc659cb
fgdamasceno <fgdamasceno@gmail.com>
2026-06-23 15:17:00 -0300

    interface predicate

--- sec18_02-predicate/src/application/Program.java
+++ sec18_02-predicate/src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -17,8 +18,13 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 
 		double min = 100.0;
+		Predicate<Product> pred = p -> p.getPrice() >= min;
 
-		list.removeIf(p -> p.getPrice() >= min);
+//		list.removeIf(p -> p.getPrice() >= min);		
+//		list.removeIf(new ProductPredicate());		
+//		list.removeIf(Product::staticProductPredicate);
+//		list.removeIf(Product::nonStaticProductPredicate);
+		list.removeIf(pred);
 
 		for (Product p : list) {
 			System.out.println(p);
